9a857bfdd08f4935ec764f4eee6c039bc313b323
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-09-27 13:41:08 +0530

    Time: 0 ms (100%), Space: 32.5 MB (79.97%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int>mappp;
-        mappp[0]=1;
-        int prefix=0;
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            prefix+=nums[i];
-            int remove=prefix-goal;
-            count+=mappp[remove];
-            mappp[prefix]+=1;
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+private:
+    int atMost(const vector<int>& nums, int k) {
+        if (k < 0)
+            return 0;
+        int left = 0, sum = 0, count = 0;
+        for (int right = 0; right < (int)nums.size(); ++right) {
+            sum += nums[right];
+            while (sum > k) {
+                sum -= nums[left++];
+            }
+            count += right - left + 1;
         }
         return count;
     }
 };
+
+// unordered_map<int,int>mappp;
+// mappp[0]=1;
+// int prefix=0;
+// int count=0;
+// for(int i=0;i<nums.size();i++){
+//     prefix+=nums[i];
+//     int remove=prefix-goal;
+//     count+=mappp[remove];
+//     mappp[prefix]+=1;
+// }
+// return count;
+
+// THIS APPROACH WORKS BUT USES EXTRA SPACE FOR HASHING
+// THIS CAN FURTHER BE REDUCED TO SC=O(1);
+// THIS QS IS SIMILAR TO SUBARRAY SUM EQUALS TO K;
