be8662f7dd8cb65fb592803cd95907b3783440ea
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-09-28 19:35:59 +0530

    Time: 0 ms (100%), Space: 14.9 MB (99.2%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int i = 0;
+        int j = 1;
+        int n = nums.size();
+        while (i < n && j < n) {
+            if (nums[j] - nums[i] == k) {
+                ans++;
+                j++;
+                i++;
+                while (j < n && nums[j] == nums[j - 1]) {
+                    j++;
+                }
+
+            } else if (nums[j] - nums[i] > k) {
+                i++;
+                if (nums[j] - nums[i] == 0) {
+                    j++;
+                }
+
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
