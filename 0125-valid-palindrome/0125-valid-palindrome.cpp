af135dc298ac3c8540884b5cc1efbcc85541d811
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-10-15 18:37:17 +0530

    Time: 0 ms (100%), Space: 9.6 MB (99.43%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool alpha(char ch){
+        if((ch>='0' && ch <= '9') || (tolower(ch)>='a' && tolower(ch)<='z'))
+        {
+            return true;
+        } 
+        return false;
+    }
+    bool isPalindrome(string s) {
+       int st=0;
+       int end=s.length()-1;
+       while(st<end){
+        if(!alpha(s[st])){
+            st++;
+            continue;
+        }
+        if(!alpha(s[end])){
+            end--;
+            continue;
+        }
+        if(tolower(s[st])!=tolower(s[end])){
+            return false;
+        }
+        st++;
+        end--;
+
+       }
+       return true; 
+    }
+};
