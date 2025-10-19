f98b70015eeb867c8f519067b745424a7f22e6b0
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-10-19 20:30:20 +0530

    Time: 3 ms (97.3%), Space: 9.4 MB (80.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    int expand(string &s, int left, int right) {
+        int n = s.size();
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
     string longestPalindrome(string s) {
         int n = s.size();
         if (n <= 1) return s;
 
         int start = 0, maxLen = 1;
 
-        auto expand = [&](int left, int right) {
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                left--;
-                right++;
-            }
-            return right - left - 1;  // palindrome length
-        };
-
         for (int i = 0; i < n; i++) {
-            int len1 = expand(i, i);       // odd-length palindrome
-            int len2 = expand(i, i + 1);   // even-length palindrome
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
             int len = max(len1, len2);
 
             if (len > maxLen) {
