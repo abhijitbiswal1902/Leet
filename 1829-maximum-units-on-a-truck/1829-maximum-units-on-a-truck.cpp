d0d6767b048e21d50622ac6ddec968f1a7617a8f
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-10-22 08:25:50 +0530

    Time: 3 ms (63.7%), Space: 20 MB (44.77%) - LeetHub

--- 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(), boxTypes.end(),
-         [](const vector<int>& a, const vector<int>& b) {
-             return a[1] > b[1];  
-         });
-         int ans=0;
-         for(int i=0;i<boxTypes.size();i++){
-             if (truckSize == 0) break;
+        sort(boxTypes.begin(),boxTypes.end(),
+        [](const vector<int>&a,const vector<int>&b){
+            return a[1]>b[1];
+        }
+        );
+
+        int ans=0;
+        // int n=boxTypes[0].size();
+        for(int i=0;i<boxTypes.size();i++){
             if(boxTypes[i][0]<=truckSize){
                 ans+=boxTypes[i][0]*boxTypes[i][1];
                 truckSize-=boxTypes[i][0];
             }
             else if(boxTypes[i][0]>truckSize){
-                ans += truckSize * boxTypes[i][1];
-                truckSize = 0;
+                ans+=truckSize*boxTypes[i][1];
+                truckSize=0;
             }
-
-         }
-         return ans;
+        }
+        return ans;
     }
 };
