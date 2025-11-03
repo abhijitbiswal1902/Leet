66236420e10639b5720902b4a803d2c6c57b0641
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-11-04 00:28:57 +0530

    Time: 0 ms (100%), Space: 19.9 MB (66.61%) - LeetHub

--- 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(),boxTypes.end(),
-        [](const vector<int>&a,const vector<int>&b){
-            return a[1]>b[1];
-        }
-        );
+        sort(boxTypes.begin(), boxTypes.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[1] > b[1];
+             });
+        int ans = 0;
 
-        int ans=0;
-        // int n=boxTypes[0].size();
-        for(int i=0;i<boxTypes.size();i++){
-            if(boxTypes[i][0]<=truckSize){
-                ans+=boxTypes[i][0]*boxTypes[i][1];
-                truckSize-=boxTypes[i][0];
-            }
-            else if(boxTypes[i][0]>truckSize){
-                ans+=truckSize*boxTypes[i][1];
+        for (int i = 0; i < boxTypes.size(); i++) {
+            if (boxTypes[i][0]<=truckSize) {
+                ans += (boxTypes[i][0] * boxTypes[i][1]);
+                truckSize = truckSize - boxTypes[i][0];
+
+            } else if (boxTypes[i][0]>truckSize) {
+                ans += truckSize * boxTypes[i][1];
                 truckSize=0;
             }
         }
