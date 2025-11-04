ce11f4f72b5689196a78aaa3d461c5781327d527
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-11-04 12:23:47 +0530

    Time: 3 ms (81.68%), Space: 11.5 MB (64.84%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int>ans;
-        vector<int>hash(26,0);
-        vector<int>phash(26,0);
-        int window=p.size();
-        int len=s.size();
-        if(len<window )return ans;
-        int left=0;
-        int right=0;
-        while(right<window){
-            phash[p[right]-'a']+=1;
-            hash[s[right]-'a']+=1;
-            right++;
+        int n = s.size(), m = p.size();
+        if (n < m) return {};
+
+        vector<int> freqP(26, 0), freqS(26, 0);
+        vector<int> ans;
+
+        // Step 1: Build frequency for 'p' and first window of 's'
+        for (int i = 0; i < m; i++) {
+            freqP[p[i] - 'a']++;
+            freqS[s[i] - 'a']++;
         }
-        right--;
-        while(right<len){
-            if(phash==hash) ans.push_back(left);
-            right++;
-            if(right==len) break;
-            hash[s[right]-'a']+=1;
-            hash[s[left]-'a']-=1;
-            left++;
+
+        // Step 2: Slide the window across 's'
+        for (int i = m; i < n; i++) {
+            if (freqP == freqS)
+                ans.push_back(i - m);  // store start index
+
+            // Slide the window:
+            freqS[s[i] - 'a']++;      // include next char
+            freqS[s[i - m] - 'a']--;  // remove previous char
         }
+
+        // Check last window
+        if (freqP == freqS)
+            ans.push_back(n - m);
+
         return ans;
     }
 };
