181cfb08707727bf82355a7d2292881d7429854b
Abhijit biswal <138918876+abhijitbiswal1902@users.noreply.github.com>
2025-11-04 09:14:03 +0530

    Time: 0 ms (100%), Space: 15 MB (77.14%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         int m=mat.size();
         int n=mat[0].size();
-        if(m*n != r*c) return mat;
+        if( m*n != r*c) return mat;
         vector<vector<int>>result(r,vector<int>(c));
         for(int i=0;i<m*n;i++){
             int oldrow=i/n;
